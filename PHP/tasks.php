15b8f8aef3e1ff95d4b47e93963314b71e2850ea
elseElseIf <mihail.pryka@gmail.com>
2023-06-29 23:02:33 +0300

    feat: add 2 PHP task, created TextInput and NumericInput classes

--- PHP/tasks.php
+++ PHP/tasks.php
@@ -26,4 +26,28 @@ class Pipeline {
 //     }
 //   );
 //   echo $result(3);
+
+
+// 2 zadanie
+
+class TextInput {
+    protected $value = '';
+
+    public function add($text) {
+        $this->value .= $text;
+    }
+
+    public function getValue() {
+        return $this->value;
+    }
+}
+
+class NumericInput extends TextInput {
+    public function add($text) {
+        if (is_numeric($text)) {
+            $this->value .= $text;
+        }
+    }
+}
+
 ?>
