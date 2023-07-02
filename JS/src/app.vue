e27fbbc149ec640f161eff3cc0a588e297a60e5f
elseElseIf <mihail.pryka@gmail.com>
2023-07-02 14:52:24 +0300

    feat: add an authorization page

---
+++ JS/src/app.vue
@@ -0,0 +1,20 @@
+<template>
+  <form @submit.prevent="login">
+    <label>Email</label>
+    <input type="text" v-model="email" />
+    <label>Password</label>
+    <input type="password" v-model="password" />
+    <button type="submit">Login</button>
+  </form>
+</template>
+
+<script>
+import { defineComponent } from 'vue';
+import { defineStore } from 'pinia';
+
+
+export default defineComponent({
+    name: 'App'
+
+});
+</script>
